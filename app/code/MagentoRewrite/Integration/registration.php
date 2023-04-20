1dfb18ef400b0bf34095068deefd7641594455ef
め 朕     有恃无恐 <820448291@qq.com>
2023-04-20 17:35:18 +0800

    重写获取用户信息接口

---
+++ app/code/MagentoRewrite/Integration/registration.php
@@ -0,0 +1,7 @@
+<?php
+   use  \Magento\Framework\Component\ComponentRegistrar;
+    ComponentRegistrar::register(
+        ComponentRegistrar::MODULE,
+        'MagentoRewrite_Integration',
+        __DIR__
+    );
